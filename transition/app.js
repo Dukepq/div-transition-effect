04af3e0ddf2a5b18c00341d5ccff19ba821c8481
Dukepq <126978164+Dukepq@users.noreply.github.com>
2023-03-15 00:05:27 +0100

    Add files via upload

---
+++ transition/app.js
@@ -0,0 +1,20 @@
+
+const wrapper = document.querySelector('.wrapper-div')
+const leftDiv = document.querySelector('.left-div')
+const rightDiv = document.querySelector('.right-div')
+
+// wrapper.addEventListener('mousemove', (e) => {
+//     console.log(e.clientX)
+//     const x = e.clientX
+//     // const percentage = x / window.innerWidth
+//     // console.log(percentage)
+//     leftDiv.style.width = `${x}px`
+// })
+
+const whenMoving = (event) => {
+    const x = event.clientX
+    leftDiv.style.width = `${x}px`
+}
+
+wrapper.onmousemove = event => whenMoving(event)
+wrapper.ontouchmove = event => whenMoving(event)
